20da774315d38f48d90da936ed1560d901ce5018
pjwin <winterspj@gmail.com>
2023-06-22 20:47:16 -0400

    concat script, change file extentions, faderbank display now working

---
+++ src/makeini.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+output_file="droid.ini"  # Specify the name of the output file
+file_list=("basicmenus.txt" "faderbanks.txt")  # List of files to concatenate
+
+# Generate a timestamp suffix
+timestamp=$(date +"%Y%m%d%H%M%S")
+
+# Check if the output file already exists
+if [ -f "$output_file" ]; then
+    # Rename the existing output file with timestamp suffix
+    mv "$output_file" "$output_file.$timestamp"
+fi
+
+# Concatenate the files
+for file in "${file_list[@]}"; do
+    cat "$file" >> "$output_file"
+done
+
+echo "Concatenation complete. Output saved to $output_file."
